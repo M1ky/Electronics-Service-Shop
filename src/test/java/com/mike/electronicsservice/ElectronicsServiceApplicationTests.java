e827d6e4f82fcc97c6b3a894e20cadd9fc10fd07
M1ky <michal.kuban2@gmail.com>
2020-08-12 17:51:11 +0200

    Initialized repository + basic configuration

---
+++ src/test/java/com/mike/electronicsservice/ElectronicsServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.mike.electronicsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsServiceApplicationTests {
+
+
+}
